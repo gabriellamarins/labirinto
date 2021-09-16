084d0b764d6f9cc24434d11945341fd184e1081a
Your Name <gabriella.marinsesilva@le-campus-numerique.fr>
2021-09-16 21:13:51 +0200

    DFS and BFS trial (stopping on 4,3 and 4,2)

--- neighbors.js
+++ neighbors.js
@@ -293,96 +293,124 @@ for (let element of lab1) {
 let case1 = document.getElementById("x0 y0").style.backgroundColor= "#FF8633";
 let case_last = document.getElementById("x4 y4").style.backgroundColor = "#FFC300"
 
-// let posX = position.posX;
-// let posY = position.posY
-// let posXlast = lab1[lab1.length - 1].posX;
-// let posYlast = lab1[lab1.length - 1].posY;
-
-async function solving_lab(posX, posY) {
-
-    let lab_temp = [...lab1];
-
-    let current = lab_temp[0];
+// let currentPosition = lab1[0];
+//
+// currentPosition = lab1.find(element => element.posX === currentPosition.posX && element.posY === currentPosition.posY)
+//
+// // console.log("  position de X : " + currentPosition.posX + "  position de Y : " + currentPosition.posY + 1)
+// console.log(currentPosition)
 
 
-    let x = current.posX;
-    let y = current.posY;
-
-    while (!(x === lab1[lab1.length - 1].posX && y === lab1[lab1.length - 1].posY)) {
-        // for (let i = 0; i < 20; i++) {
-
-        current.status = "checked"
-        // current.checked=true
-       // console.log(current)
-        let number_walls = current.walls.filter(wall => wall === true).length
-
-
-
-        if (number_walls <= 1) {
-            console.log("Intersection")
-            current.repere = "intersection"
-          //  document.getElementById("x" + " " + current.posX + " " + "y" + " " + current.posY).style.backgroundColor = "E0CCCD"
-
-        } else if (number_walls === 3) {
-            if (!(current === lab_temp[0])) {
-                current.repere = "Cul-de-sac"
-                console.log("Cul-de-sac")
-                current = document.getElementById("x" + " " + current.posX + " " + "y" + " " + current.posY);
-              //  current.style.backgroundColor = "F3A682"
-                let intersectionList = lab1.filter(el => el.repere === "intersection")// cria uma nova array com as intersecções
-                current = intersectionList.find(el => el.repere === "intersection")
-             //   console.log(current);
-                intersectionList.shift(); // isso elimina o primeiro elemento da array criada pra que ele não seja solicitado de novo
-                console.log(intersectionList);
-                current.repere = "undefined";
-                x = current.posX;
-                y = current.posY;
-                //console.log(current);
-                //console.log(intersectionList)
+function getNeighbor (posX, posY) {
+    let neighbors = []
+    const currentPosition = lab1.find(el => el.posX === posX && el.posY === posY);
+    for (let i = 0; i < currentPosition.walls.length; i++) {
+        let neighbor;
+        if (!currentPosition.walls[i]) {
+            if (i === 0) {
+                const x = currentPosition.posX
+                const y = currentPosition.posY - 1
+               neighbor = lab1.find(el => el.posX === x && el.posY === y);
+      //          neighbors.push(neighbor)
             }
+            if (i === 1) {
+                const x = currentPosition.posX + 1
+                const y = currentPosition.posY
+                neighbor = lab1.find(el => el.posX === x && el.posY === y);
+      //          neighbors.push(neighbor)
+            }
+            if (i === 2) {
+                const x = currentPosition.posX
+                const y = currentPosition.posY + 1
+                neighbor = lab1.find(el => el.posX === x && el.posY === y);
+             //   neighbors.push(neighbor)
+            }
+            if (i === 3) {
+                const x = currentPosition.posX - 1
+                const y = currentPosition.posY
+                neighbor = lab1.find(el => el.posX === x && el.posY === y);
+                // neighbors.push(neighbor)
+            }
+            if (!neighbor.visited) {
+                 neighbors.push (neighbor)
+            }
+
         }
 
-        if (current.walls[0] === false && lab1.find(el => el.posY === y - 1 && el.posX === x).status === undefined) { //top
-
-            y = y - 1;
-
-        } else if (current.walls[1] === false && lab1.find(el => el.posX === x + 1 && el.posY === y).status === undefined) { //right
-
-            x = x + 1;
-
-        } else if (current.walls[2] === false && lab1.find(el => el.posY === y + 1 && el.posX === x).status === undefined) { //down
-
-            y = y + 1;
-
-        } else if (current.walls[3] === false && lab1.find(el => el.posX === x - 1 && el.posY === y).status === undefined) { //left
-
-            x = x - 1;
-        }
-
-        current = lab1.find(el => el.posX === x && el.posY === y)
-
-         console.log(current);
-        // await delay(200)
-        // displayAvancement(current);
     }
-
-    console.log("Félicitations ! Tu t'es échappé !");
+  //  console.log(currentPosition)
+  //  console.log(neighbors)
+    return neighbors
 
 }
+//getNeighbor(0, 3);
 
-solving_lab();
 
-// function displayAvancement(position) {
-//     position = document.getElementById("x" + " " + position.posX + " " + "y" + " " + position.posY)
-//   //  position.style.backgroundColor = "FAD5C4"
+//DFS
+//
+// async function DFS(lab1) {
+//     //     // Utiliser le posX & posY
+//     var currentPosition = lab1[0];
+//     let stack = [];
+//     stack.push(currentPosition);
+//     var neighbors;
+//
+//     var finish = lab1[lab1.length - 1]
+//     currentPosition.visited = false;
+//
+//     while (stack.length > 0) {
+//         // .top retourne le derier élément de la pile (stack)
+//         currentPosition = stack.pop();
+//         currentPosition.visited = true;
+//
+//         if (currentPosition.posX === finish.posX && currentPosition.posY === finish.posY) {
+//             //    console.log(stack);
+//             return stack;
+//         }
+//
+//
+//         neighbors = getNeighbor(currentPosition.posX, currentPosition.posY)
+//         for (let neighbor of neighbors) {
+//             if (neighbor !== currentPosition.visited) {
+//                 stack.push(neighbor);
+//             }
+//
+//         }
+//         console.log(currentPosition)
+//     }
 //
 // }
-//
-// function delay(delayInms) {
-//     return new Promise(resolve => {
-//         setTimeout(() => {
-//             resolve(2);
-//         }, delayInms);
-//     });
-// }
 
+// console.log(DFS(lab1))
+
+
+//////
+
+async function BFS(lab1) { //
+    // usar o posx e o posy
+    let currentPosition = lab1[0];
+    let queue = [];
+    queue.push(currentPosition);
+    let neighbors;
+    let finish = lab1[lab1.length - 1];
+    currentPosition.visited = false;
+    while (queue.length > 0) {
+        currentPosition = queue.shift();
+        currentPosition.visited = true;
+     //   await sleep(200)
+    //    colorPosition(currentPosition.posX, currentPosition.posY)
+        if (currentPosition.posX === finish.posX && currentPosition.posY === finish.posY) {
+           //  console.log(queue);
+            return queue;
+        }
+        neighbors = getNeighbor(currentPosition.posX, currentPosition.posY)
+        for (let neighbor of neighbors) {
+            if (neighbor !== currentPosition.visited) {
+                queue.push(neighbor);
+            }
+        }
+        console.log(currentPosition)
+    }
+}
+
+console.log(BFS(lab1))
